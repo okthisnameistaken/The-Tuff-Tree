2e0effcd588eeba3080f581de7763afbe023c747
okthisnameistaken <147211860+okthisnameistaken@users.noreply.github.com>
2026-04-13 17:50:54 -0500

    Reboot, buyable, new upgrades,
    im on a roll my boi

--- js/layers.js
+++ js/layers.js
@@ -5,6 +5,8 @@ addLayer("r", {
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
+		buyable11Unlocked: false,
+		upgrade15Unlocked: false,
     }},
     color: "#d35202",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
@@ -16,6 +18,9 @@ addLayer("r", {
     gainMult() { // Calculate the multiplier for main currency from bonuses
         let mult = new Decimal(1)
         if (hasUpgrade('r', 13)) mult = mult.times(upgradeEffect('r', 13))
+        if (player['b'] && player['b'].points) {
+            mult = mult.times(buyableEffect('b', 11))
+        }
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -26,6 +31,20 @@ addLayer("r", {
         {key: "r", description: "Rebirths: You can rebirth for tokens!", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
+    update() {
+        if (player['r'].points.gte(10)) {
+            player['r'].buyable11Unlocked = true
+            player['r'].upgrade15Unlocked = true
+        }
+        if (hasUpgrade('r', 15)) {
+            let owned = getBuyableAmount('r', 11)
+            let cost = new Decimal(4).pow(owned)
+            if (player['r'].points.gte(cost)) {
+                player['r'].points = player['r'].points.sub(cost)
+                setBuyableAmount('r', 11, owned.add(1))
+            }
+        }
+    },
 
         upgrades: { 11: { title: "The first upgrade",
              description: "Boost ticks by 2x.",
@@ -51,5 +70,100 @@ addLayer("r", {
             description: "Boost ticks by 3x.",
             cost: new Decimal(10),
             },
+            15: { title: "Sweet automation",
+            description: "Automatically buy the Tripler whenever you can.",
+            cost: new Decimal(50),
+            unlocked() { return player[this.layer].upgrade15Unlocked },
+            },
+    },
+    buyables: {
+    11: {
+        title: "Tripler",
+        unlocked() { return player[this.layer].buyable11Unlocked },
+        cost(x) { 
+            return new Decimal(4).pow(x) 
+        },
+        effect(x) { return new Decimal(3).pow(x) },
+        effectDisplay() { return format(this.effect(getBuyableAmount(this.layer, this.id))) + "x" },
+        display() { 
+            let owned = getBuyableAmount(this.layer, this.id)
+            let cost = this.cost(owned)
+            return `Cost: ${format(cost)}<br>Effect: Multiply ticks by ${format(this.effect(owned))}x<br>Owned: ${owned}`
+        },
+        canAfford() { 
+            let owned = getBuyableAmount(this.layer, this.id)
+            return player[this.layer].points.gte(this.cost(owned)) 
+        },
+        buy() {
+            let owned = getBuyableAmount(this.layer, this.id)
+            let cost = this.cost(owned)
+            player[this.layer].points = player[this.layer].points.sub(cost)
+            setBuyableAmount(this.layer, this.id, owned.add(1))
+        },
+    },
+}
+})
+
+addLayer("b", {
+    name: "reboot",
+    symbol: "B",
+    position: 0,
+    startData() { return {
+        unlocked: false,
+        points: new Decimal(0),
+        rebootUnlocked: false,
+    }},
+    color: "#4169e1",
+    requires: new Decimal(100000000), // 100 million
+    resource: "points",
+    baseResource: "ticks",
+    baseAmount() { return player.points },
+    type: "normal",
+    exponent: 0.5,
+    gainMult() {
+        return new Decimal(1)
+    },
+    gainExp() {
+        return new Decimal(1)
+    },
+    row: 1,
+    update() {
+        if (player.points.gte(100000000)) player['b'].rebootUnlocked = true
+    },
+    layerShown() { return player['b'].rebootUnlocked },
+    getResetGain() {
+        if (player.points.lt(100000000)) return new Decimal(0)
+        let gain = player.points.div(100000000).pow(0.5).times(5)
+        return gain.floor()
+    },
+    buyables: {
+        11: {
+            title: "Token Doubler",
+            cost(x) {
+                if (x.lt(2)) return new Decimal(2).mul(new Decimal(3).pow(x))
+                else return new Decimal(6).mul(new Decimal(5).pow(x.sub(1)))
+            },
+            effect(x) {
+                return new Decimal(2).pow(x)
+            },
+            effectDisplay() {
+                return format(this.effect(getBuyableAmount(this.layer, this.id))) + "x"
+            },
+            display() {
+                let owned = getBuyableAmount(this.layer, this.id)
+                let cost = this.cost(owned)
+                return `Cost: ${format(cost)}<br>Effect: Multiply tokens by ${format(this.effect(owned))}x<br>Owned: ${owned}`
+            },
+            canAfford() {
+                let owned = getBuyableAmount(this.layer, this.id)
+                return player[this.layer].points.gte(this.cost(owned))
+            },
+            buy() {
+                let owned = getBuyableAmount(this.layer, this.id)
+                let cost = this.cost(owned)
+                player[this.layer].points = player[this.layer].points.sub(cost)
+                setBuyableAmount(this.layer, this.id, owned.add(1))
+            },
+        },
     },
 })
--- js/mod.js
+++ js/mod.js
@@ -45,6 +45,7 @@ function getPointGen() {
 	if (hasUpgrade('r', 11)) gain = gain.times(2)
 	if (hasUpgrade('r', 12)) gain = gain.times(upgradeEffect('r', 12))
 	if (hasUpgrade('r', 14)) gain = gain.times(3)
+	gain = gain.times(buyableEffect('r', 11))
 	return gain
 }
 
